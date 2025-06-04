a79917d99efce97affd0f03252399a9217a37aa8
nauvalachid <nauvalachid@gmail.com>
2025-06-05 03:06:14 +0700

    first commit

---
+++ lib/data/repository/auth_repository.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/model/request/login_request_model.dart';
+import 'package:canary_template/data/model/request/register_request_model.dart';
+import 'package:canary_template/data/model/response/login_response_model.dart';
+import 'package:canary_template/service/service_http_client.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:dartz/dartz.dart';
+
+class AuthRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  final secureStorage = FlutterSecureStorage();
+
+  AuthRepository(this._serviceHttpClient);
+
+  Future<Either<String, LoginResponseModel>>login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'login',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = LoginResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: "authToken",
+          value: loginResponse.data!.token,
+        );
+        await secureStorage.write(
+          key: "userRole",
+          value: loginResponse.data!.role,
+        );
+        return Right(loginResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Login failed');
+      }
+    } catch (e) {
+      return Left("An error occured while logging in:");
+    }
+  }
+
+  Future<Either<String, String>>register(RegisterRequestModel requestModel) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "register",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      final registerResponse = jsonResponse['message'];
+      if (response.statusCode == 201) {
+        return Right(registerResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Registration failed');
+      }
+    } catch (e) {
+      return Left("An error occured while registering. : $e");
+    }
+  }
+}
---
+++ lib/service/service_http_client.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.0.0.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  // POST
+  Future<http.Response> post(String endpoint, Map <String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endpoint");
+    
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+          } catch (e) {
+            throw Exception("POST request failed: $e");
+          }
+  }      
+  
+  // GET
+  Future<http.Response> get(String endpoint) async {
+    final token = await secureStorage.read(key: "token");
+    final url = Uri.parse("$baseUrl$endpoint");
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          if (token != null) 'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception("GET request failed: $e");
+    }
+  }
+
+  // PUT
+}
