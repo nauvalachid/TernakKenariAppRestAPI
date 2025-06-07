d32661285fc43a36f42984134f9d4c066584b7ea
nauvalachid <nauvalachid@gmail.com>
2025-06-08 00:50:51 +0700

    menambahkan model IndukRequest dengan metode serialisasi JSON

---
+++ lib/data/model/request/admin/induk_request_model.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+class IndukRequestModel {
+    final String? noRing;
+    final DateTime? tanggalLahir;
+    final String? jenisKelamin;
+    final String? jenisKenari;
+    final String? keterangan;
+    final dynamic gambarInduk;
+
+    IndukRequestModel({
+        this.noRing,
+        this.tanggalLahir,
+        this.jenisKelamin,
+        this.jenisKenari,
+        this.keterangan,
+        this.gambarInduk,
+    });
+
+    factory IndukRequestModel.fromJson(String str) => IndukRequestModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory IndukRequestModel.fromMap(Map<String, dynamic> json) => IndukRequestModel(
+        noRing: json["no_ring"],
+        tanggalLahir: json["tanggal_lahir"] == null ? null : DateTime.parse(json["tanggal_lahir"]),
+        jenisKelamin: json["jenis_kelamin"],
+        jenisKenari: json["jenis_kenari"],
+        keterangan: json["keterangan"],
+        gambarInduk: json["gambar_induk"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "no_ring": noRing,
+        "tanggal_lahir": "${tanggalLahir!.year.toString().padLeft(4, '0')}-${tanggalLahir!.month.toString().padLeft(2, '0')}-${tanggalLahir!.day.toString().padLeft(2, '0')}",
+        "jenis_kelamin": jenisKelamin,
+        "jenis_kenari": jenisKenari,
+        "keterangan": keterangan,
+        "gambar_induk": gambarInduk,
+    };
+}
