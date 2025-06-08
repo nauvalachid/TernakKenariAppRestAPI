066e8cf3573c5df3b224ae4f850b4c6a4184f078
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:04:45 +0700

    menambahkan kelas IndukRepository dengan dependensi ServiceHttpClient

---
+++ lib/data/repository/induk_repository.dart
@@ -0,0 +1,7 @@
+import 'package:canary_template/service/service_http_client.dart';
+
+class IndukRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  IndukRepository(this._serviceHttpClient);
+}
