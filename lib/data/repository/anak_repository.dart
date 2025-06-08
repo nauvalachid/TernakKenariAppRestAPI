bcaf81ca8a272066a498c35e07117f300f6ab560
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:12:30 +0700

    menambahkan metode addAnak pada kelas AnakRepository untuk menambahkan anak dengan permintaan POST

--- lib/data/repository/anak_repository.dart
+++ lib/data/repository/anak_repository.dart
@@ -1,7 +1,34 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/model/request/admin/anak_request_model.dart';
+import 'package:canary_template/data/model/response/get_all_anak_response.dart';
 import 'package:canary_template/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 
 class AnakRepository {
   final ServiceHttpClient _serviceHttpClient;
 
   AnakRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetAnakById>> addAnak(
+    AnakRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "admin/anak",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAnakById.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
 }
