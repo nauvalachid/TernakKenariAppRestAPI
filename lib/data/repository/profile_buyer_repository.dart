33581aaa7d0a6cd3f7295e3b7fc42b77193abd44
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:21:42 +0700

    menambahkan metode addProfileBuyer pada kelas ProfileBuyerRepository untuk menambahkan profil pembeli

--- lib/data/repository/profile_buyer_repository.dart
+++ lib/data/repository/profile_buyer_repository.dart
@@ -1,6 +1,36 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/model/request/buyer/buyer_profile_request_model.dart';
+import 'package:canary_template/data/model/response/buyer/buyer_profile_response_model.dart';
 import 'package:canary_template/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 
 class ProfileBuyerRepository {
   final ServiceHttpClient _serviceHttpClient;
   ProfileBuyerRepository(this._serviceHttpClient);
+
+   Future<Either<String, BuyerProfileResponseModel>> addProfileBuyer(
+    BuyerProfileRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "buyer/profile",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = BuyerProfileResponseModel.fromJson(
+          jsonResponse,
+        );
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+
 }
