ebfc437e20c925cc3ca9935fd66c7c3d09f534e7
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:11:42 +0700

    menambahkan kelas AnakRepository dengan dependensi ServiceHttpClient

---
+++ lib/data/repository/anak_repository.dart
@@ -0,0 +1,7 @@
+import 'package:canary_template/service/service_http_client.dart';
+
+class AnakRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  AnakRepository(this._serviceHttpClient);
+}
