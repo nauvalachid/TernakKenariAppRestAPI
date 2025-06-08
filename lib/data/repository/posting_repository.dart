c3d3f1d152be07ef4ca11d5cb0769b979da098b2
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:16:29 +0700

    menambahkan metode getAllBurung pada kelas PostingRepository untuk mengambil semua burung

--- lib/data/repository/posting_repository.dart
+++ lib/data/repository/posting_repository.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:canary_template/data/model/request/admin/posting_jual_request_model.dart';
 import 'package:canary_template/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canary_template/data/model/response/get_all_burung_response_model.dart';
 import 'package:canary_template/service/service_http_client.dart';
 import 'package:dartz/dartz.dart';
 
@@ -32,6 +33,23 @@ class PostingRepository {
     } catch (e) {
       log("Error in Add burung : $e");
       return Left("An error occurred while post burung: $e");
+    }
+  }
+
+   Future<Either<String, GetAllBurungModel>> getAllBurung() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/burung-semua");
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final getAllBurung = GetAllBurungModel.fromMap(jsonResponse);
+
+        return Right(getAllBurung);
+      } else {
+        final jsonResponse = json.decode(response.body);
+        return Left(jsonResponse['message'] ?? "Get All burung failed");
+      }
+    } catch (e) {
+      return Left("An error occurred while getting all burung: $e");
     }
   }
 }
