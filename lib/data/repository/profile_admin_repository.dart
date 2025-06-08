089f6594f13a5288e9a7f8ddb9eeda75f6cdf911
nauvalachid <nauvalachid@gmail.com>
2025-06-08 23:51:07 +0700

    menambahkan kelas ProfileAdminRepository dengan dependensi ServiceHttpClient dan FlutterSecureStorage

---
+++ lib/data/repository/profile_admin_repository.dart
@@ -0,0 +1,7 @@
+import 'package:canary_template/service/service_http_client.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ProfileAdminRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  final secureStorage = FlutterSecureStorage();
+}
