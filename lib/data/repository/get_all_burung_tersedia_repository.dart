fa4fb6530c05eab59bb4e2c71f981741e1468232
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:18:40 +0700

    menambahkan kelas GetAllBurungTersediaRepository dengan dependensi ServiceHttpClient

---
+++ lib/data/repository/get_all_burung_tersedia_repository.dart
@@ -0,0 +1,6 @@
+import 'package:canary_template/service/service_http_client.dart';
+
+class GetAllBurungTersediaRepository {
+  final ServiceHttpClient httpClient;
+  GetAllBurungTersediaRepository(this.httpClient);
+}
