5a436a290820aa4e90abea3ef06450c1a7e53f45
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:14:46 +0700

    menambahkan kelas PostingRepository dengan dependensi ServiceHttpClient

---
+++ lib/data/repository/posting_repository.dart
@@ -0,0 +1,6 @@
+import 'package:canary_template/service/service_http_client.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  PostingRepository(this._serviceHttpClient);
+}
