5d8d5ea47b761f91936d8cfbb4ede3d0c772a802
nauvalachid <nauvalachid@gmail.com>
2025-06-09 00:20:53 +0700

    menambahkan kelas ProfileBuyerRepository dengan dependensi ServiceHttpClient

---
+++ lib/data/repository/profile_buyer_repository.dart
@@ -0,0 +1,6 @@
+import 'package:canary_template/service/service_http_client.dart';
+
+class ProfileBuyerRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  ProfileBuyerRepository(this._serviceHttpClient);
+}
