7fdab63e049f759a0fa3081d2194526c6a2d3345
nauvalachid <nauvalachid@gmail.com>
2025-06-10 22:56:34 +0700

    Menambahkan implementasi metode build pada widget ProfileViewBuyer

--- lib/presentation/buyer/profile/widget/profile_view_buyer.dart
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -4,4 +4,17 @@ import 'package:flutter/material.dart';
 class ProfileViewBuyer extends StatelessWidget {
   final Data profile;
   const ProfileViewBuyer({super.key, required this.profile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
 }
