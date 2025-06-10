3fcbc2448d41780455815a9d5b27603137938ebe
nauvalachid <nauvalachid@gmail.com>
2025-06-10 23:43:26 +0700

    Menambahkan kelas state GetBurungTersediaLoaded untuk menangani data burung yang tersedia

--- lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_state.dart
+++ lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_state.dart
@@ -1,5 +1,12 @@
+import 'package:canary_template/data/model/response/burung_semua_tersedia_model.dart';
+
 sealed class GetBurungTersediaState {}
 
 final class GetBurungTersediaInitial extends GetBurungTersediaState {}
 
 final class GetBurungTersediaLoading extends GetBurungTersediaState {}
+
+final class GetBurungTersediaLoaded extends GetBurungTersediaState {
+  final BurungSemuaTersediaModel burungTersedia;
+  GetBurungTersediaLoaded({required this.burungTersedia});
+}
