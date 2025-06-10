660bffe0f9680ec19f37f6a3a7572fee8abe30ce
nauvalachid <nauvalachid@gmail.com>
2025-06-10 21:16:22 +0700

    Implement RegisterBloc to handle registration events and states

--- lib/presentation/auth/bloc/register/register_bloc.dart
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -8,4 +8,18 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
     on<RegisterRequested>(_onRegisterRequested);
   }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r) => emit(RegisterSuccess(responseModel: r)),
+  );
+}
 }
