b83357090e92551c4036e0cf2f85b733e62b1390
nauvalachid <nauvalachid@gmail.com>
2025-06-10 23:35:38 +0700

    Menambahkan kelas BuyerProfileScreen untuk mengelola tampilan profil pembeli

---
+++ lib/presentation/buyer/profile/buyer_profile_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BuyerProfileScreen extends StatefulWidget {
+  const BuyerProfileScreen({super.key});
+
+  @override
+  State<BuyerProfileScreen> createState() => _BuyerProfileScreenState();
+}
