f134f4e1780b6441ae0078b223c2353270883496
nauvalachid <nauvalachid@gmail.com>
2025-06-10 22:42:17 +0700

    Menambahkan kelas ProfileBuyerLoaded untuk mengelola status profil pembeli yang dimuat

--- lib/presentation/buyer/profile/bloc/profile_buyer_state.dart
+++ lib/presentation/buyer/profile/bloc/profile_buyer_state.dart
@@ -1,5 +1,13 @@
+import 'package:canary_template/data/model/response/buyer/buyer_profile_response_model.dart';
+
 sealed class ProfileBuyerState {}
 
 final class ProfileBuyerInitial extends ProfileBuyerState {}
 
 final class ProfileBuyerLoading extends ProfileBuyerState {}
+
+final class ProfileBuyerLoaded extends ProfileBuyerState {
+  final BuyerProfileResponseModel profile;
+
+  ProfileBuyerLoaded({required this.profile});
+}
