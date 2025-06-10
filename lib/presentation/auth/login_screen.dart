8411e6c43744dc8ce1c170dda29d295dafb59ed3
nauvalachid <nauvalachid@gmail.com>
2025-06-10 21:29:02 +0700

    Menambahkan implementasi build untuk LoginScreen dengan struktur form dan tampilan yang sesuai

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:canary_template/core/components/spaces.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -27,5 +28,32 @@ class _LoginScreenState extends State<LoginScreen> {
     passwordController.dispose();
     _key.currentState?.dispose();
     super.dispose();
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SpaceHeight(170),
+                Text(
+                  'SELAMAT DATANG KEMBALI',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
