e77c1420cc164fcd3d34d10b820c7e851ada92c0
nauvalachid <nauvalachid@gmail.com>
2025-06-10 23:42:08 +0700

    Hapus direktif bagian yang tidak digunakan dan sesuaikan definisi kelas state dalam GetBurungTersediaState

--- lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_state.dart
+++ lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_state.dart
@@ -1,6 +1,5 @@
-part of 'get_burung_tersedia_bloc.dart';
-
-@immutable
 sealed class GetBurungTersediaState {}
 
 final class GetBurungTersediaInitial extends GetBurungTersediaState {}
+
+final class GetBurungTersediaLoading extends GetBurungTersediaState {}
