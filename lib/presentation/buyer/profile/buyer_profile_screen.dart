af257c2bd63addebd8411060c3a6a0cedcbb77d0
nauvalachid <nauvalachid@gmail.com>
2025-06-10 23:38:14 +0700

    Menambahkan logika untuk menampilkan tampilan profil pembeli setelah berhasil dimuat

--- lib/presentation/buyer/profile/buyer_profile_screen.dart
+++ lib/presentation/buyer/profile/buyer_profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:canary_template/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
 import 'package:canary_template/presentation/buyer/profile/bloc/profile_buyer_event.dart';
 import 'package:canary_template/presentation/buyer/profile/bloc/profile_buyer_state.dart';
+import 'package:canary_template/presentation/buyer/profile/widget/profile_view_buyer.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -37,6 +38,12 @@ class _BuyerProfileScreenState extends State<BuyerProfileScreen> {
           builder: (context, state) {
             if (state is ProfileBuyerLoading) {
               return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is ProfileBuyerLoaded &&
+                state.profile.data.name.isNotEmpty) {
+              final profile = state.profile.data;
+              return ProfileViewBuyer(profile: profile);
             }
           }
         ),
