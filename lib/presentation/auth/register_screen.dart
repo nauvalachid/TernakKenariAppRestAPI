fabb0f5292b0934f49857c2d6d537aa042c319ae
nauvalachid <nauvalachid@gmail.com>
2025-06-10 21:41:29 +0700

    Menambahkan tampilan awal untuk RegisterScreen

---
+++ lib/presentation/auth/register_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
