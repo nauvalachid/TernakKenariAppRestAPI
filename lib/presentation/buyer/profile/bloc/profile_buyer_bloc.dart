fff21626fd375125fc12b5ca41d22ae1d86513be
nauvalachid <nauvalachid@gmail.com>
2025-06-10 22:48:51 +0700

    Refactor ProfileBuyerBloc to improve event handling and repository integration

--- lib/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
+++ lib/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
@@ -1,13 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:canary_template/data/repository/profile_buyer_repository.dart';
+import 'package:canary_template/presentation/buyer/profile/bloc/profile_buyer_event.dart';
+import 'package:canary_template/presentation/buyer/profile/bloc/profile_buyer_state.dart';
 import 'package:meta/meta.dart';
 
-part 'profile_buyer_event.dart';
-part 'profile_buyer_state.dart';
-
 class ProfileBuyerBloc extends Bloc<ProfileBuyerEvent, ProfileBuyerState> {
-  ProfileBuyerBloc() : super(ProfileBuyerInitial()) {
-    on<ProfileBuyerEvent>((event, emit) {
-      // TODO: implement event handler
+  final ProfileBuyerRepository profileBuyerRepository;
+  ProfileBuyerBloc({required this.profileBuyerRepository}) : super(ProfileBuyerInitial()){
+    on<AddProfileBuyerEvent>(_addProfileBuyer);
+    on<GetProfileBuyerEvent>(_getProfileBUyer);
+  }
+
+  Future<void> _addProfileBuyer(
+    AddProfileBuyerEvent event,
+    Emitter<ProfileBuyerState> emit,
+  ) async {
+    emit(ProfileBuyerLoading());
+    final result = await profileBuyerRepository.addProfileBuyer(
+      event.requestModel,
+    );
+    result.fold((error) => emit(ProfileBuyerAddError(message: error)), (
+      profile,
+    ) {
+      emit(ProfileBuyerAdded(profile: profile));
     });
   }
 }
