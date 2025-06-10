3adae736990c22d0eb04db4e9d030363d186b343
nauvalachid <nauvalachid@gmail.com>
2025-06-10 22:55:45 +0700

    Menambahkan widget ProfileViewBuyer untuk menampilkan profil pembeli

---
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -0,0 +1,7 @@
+import 'package:canary_template/data/model/response/buyer/buyer_profile_response_model.dart';
+import 'package:flutter/material.dart';
+
+class ProfileViewBuyer extends StatelessWidget {
+  final Data profile;
+  const ProfileViewBuyer({super.key, required this.profile});
+}
