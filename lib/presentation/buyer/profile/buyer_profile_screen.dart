855f7073d508aea11a4e58ec174f203d3138aee1
nauvalachid <nauvalachid@gmail.com>
2025-06-10 23:36:17 +0700

    Menambahkan inisialisasi profil pembeli pada BuyerProfileScreen saat halaman dimuat

--- lib/presentation/buyer/profile/buyer_profile_screen.dart
+++ lib/presentation/buyer/profile/buyer_profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:canary_template/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class BuyerProfileScreen extends StatefulWidget {
   const BuyerProfileScreen({super.key});
@@ -6,3 +8,11 @@ class BuyerProfileScreen extends StatefulWidget {
   @override
   State<BuyerProfileScreen> createState() => _BuyerProfileScreenState();
 }
+
+class _BuyerProfileScreenState extends State<BuyerProfileScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Ambil profil pembeli saat halaman dimuat
+    context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+  }
